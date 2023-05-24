b95d6642cea909975230e8da53e0dbf743adcd5a
FlameBringer09 <134323169+FlameBringer09@users.noreply.github.com>
2023-05-24 11:05:47 -0700

    Update and rename helloWorld.java to HelloWorld.java
    consider the code

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,6 @@ public class helloWorld
     public static void main(String[] args)
     {
        System.out.println("Hello World");
-       System.out.println("This is a test");
-        System.out.println("Snorlax");
+       System.out.println("Can I post the mancala code I have finnished and tested it");
     } 
 }
